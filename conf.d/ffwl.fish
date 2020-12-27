1dba65cf07c1cce0b75346eb4654b71a1e3fbfc1
lukasjoc <jochamlu@gmail.com>
2020-12-27 14:20:45 +0100

    update

--- conf.d/ffwl.fish
+++ conf.d/ffwl.fish
@@ -1,2 +1,106 @@
-# Source all ffwl scripts before loading config.fish
-source "$HOME/.config/fish/functions/ffwl.fish"
+# Basic functions
+
+#Check for system (mac specific stuff)
+switch (uname -s)
+  case "Darwin"
+    function locate_update -d "Update locate db"
+      command sudo "/usr/libexec/locate.updatedb"
+    end
+    function runbrewstuff -d "update and upgrade brew plugins"
+      command echo "update/upgrade brew formulars..."
+      ;and brew update
+      ;and brew upgrade --greedy
+      ;and rm -rf (brew --cache)
+      ;and brew cleanup
+      ;and brew doctor
+      ;and echo "done ;)"
+    end
+    function bca -d "install brew caskroom"
+      echo "$1, $2"
+      command brew "$1" "$2" --cask
+    end
+end
+
+function ns -d "Start new default shell"
+  command exec "$SHELL" -l
+end
+
+function npmls -d "List all globally installed npm packages"
+  command npm list -g --depth=0;
+end
+
+function sc -d "List all available scripts for package.json and composer.json"
+  if test -e "$PWD/package.json"
+    command jq .scripts "package.json"
+  end
+  if test -e "$PWD/composer.json"
+    command jq .scripts "composer.json"
+  end
+end
+
+function fun -d "Go to directory $HOME/fun"
+  command cd "$HOME/fun"
+end
+
+function builds -d "Go to directory $HOME/builds"
+  command cd "$HOME/builds"
+end
+
+function gocmd -d "Go Language Helper"
+  switch "$cmd"
+    case -bench
+      command go test -v --bench . --benchmem -race
+    case -c -clean
+      command go clean -x -r -cache -modcache
+    case -mod
+      command go mod init "github.com/lukasjoc/$1"
+    case "" -h
+      echo "Usage: gocmd <option>"
+      echo "Options:"
+      echo "  -c or -clean    Clean cache and modcache"
+      echo "  -b or -bench    Test and benchmark go code"
+      echo "  -mod            Create mod init files"
+      echo "  -h              Print this help message"
+  end
+end
+
+# Taken from -> https://github.com/decors/fish-colored-man/blob/master/functions/man.fish
+function man --wraps man --description 'Format and display manual pages'
+  set -q man_blink; and set -l blink (set_color $man_blink); or set -l blink (set_color -o red)
+  set -q man_bold; and set -l bold (set_color $man_bold); or set -l bold (set_color -o 5fafd7)
+  set -q man_standout; and set -l standout (set_color $man_standout); or set -l standout (set_color 949494)
+  set -q man_underline; and set -l underline (set_color $man_underline); or set -l underline (set_color -u afafd7)
+
+  set -l end (printf "\e[0m")
+  set -lx LESS_TERMCAP_mb "$blink"
+  set -lx LESS_TERMCAP_md "$bold"
+  set -lx LESS_TERMCAP_me "$end"
+  set -lx LESS_TERMCAP_so "$standout"
+  set -lx LESS_TERMCAP_se "$end"
+  set -lx LESS_TERMCAP_us "$underline"
+  set -lx LESS_TERMCAP_ue "$end"
+  set -lx LESS '-R -s'
+
+  set -lx GROFF_NO_SGR yes # fedora
+  set -lx MANPATH (string join : $MANPATH)
+  if test -z "$MANPATH"
+      type -q manpath
+      and set MANPATH (command manpath)
+  end
+
+  # Check data dir for Fish 2.x compatibility
+  set -l fish_data_dir
+  if set -q __fish_data_dir
+    set fish_data_dir $__fish_data_dir
+  else
+    set fish_data_dir $__fish_datadir
+  end
+
+  set -l fish_manpath (dirname $fish_data_dir)/fish/man
+  if test -d "$fish_manpath" -a -n "$MANPATH"
+    set MANPATH "$fish_manpath":$MANPATH
+    command man $argv
+    return
+  end
+  command man "$argv"
+end
